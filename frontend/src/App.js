7c26fdd34f7d06d47d5bfdb5def5a9fbca15c1e7
Ruslan <nikolrusik@gmail.com>
2023-02-19 23:25:59 +0300

    Add qraphql for front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import axios from 'axios';
 import { BrowserRouter, Route, Navigate, Routes } from 'react-router-dom';
 import Cookies from 'universal-cookie';
+import { useQuery, gql, ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 
 
 // Import components
@@ -13,12 +14,25 @@ import Item from './components/Item';
 import LoginForm from './components/Login';
 
 
+
+
+const link = createHttpLink({
+  uri: 'http://localhost:8000/graphql/',
+  credentials: 'same-origin',
+});
+
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       products: [],
       token: "",
+      client: new ApolloClient({
+        link,
+        cache: new InMemoryCache(),
+        headers: this.get_headers(),
+
+      })
     }
   }
 
@@ -29,7 +43,7 @@ class App extends React.Component {
   }
 
   is_authenticated() {
-    return this.state.token != "";
+    return this.state?.token != "";
   }
 
   logout() {
@@ -47,7 +61,7 @@ class App extends React.Component {
       "Content-type": "application/json",
     };
     if (this.is_authenticated()) {
-      headers['Authorization'] = "Token" + this.state.token;
+      headers['Authorization'] = "Token" + this.state?.token;
     }
     return headers;
   }
@@ -67,20 +81,9 @@ class App extends React.Component {
 
   load_data() {
     const headers = this.get_headers();
-    axios
-      .get("http://127.0.0.1:8000/api/products/", { headers })
-      .then((response) => {
-        this.setState({
-          products: response.data,
-        });
-      })
-      .catch((error) => {
-        console.log(error)
-        this.setState({
-          products: [],
-        })
-      });
+
   }
+
 
   componentDidMount() {
     this.get_token_from_storage();
@@ -89,28 +92,30 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <BrowserRouter>
-          <Navbar
-            is_authenticated={this.is_authenticated.bind(this)}
-            logout={this.logout.bind(this)}
-          />
-          <Routes>
-            <Route
-              path="/product/:id"
-              element={<Item />} />
-            <Route
-              path="/cart"
-              element={<Cart />} />
-            <Route
-              path="/"
-              element={<Products />} />
-            <Route
-              path="/login"
-              element={<LoginForm
-                get_token={(username, password) => this.get_token(username, password)}
-              />} />
-          </Routes>
-        </BrowserRouter>
+        <ApolloProvider client={this.state.client}>
+          <BrowserRouter>
+            <Navbar
+              is_authenticated={this.is_authenticated.bind(this)}
+              logout={this.logout.bind(this)}
+            />
+            <Routes>
+              <Route
+                path="/product/:id"
+                element={<Item />} />
+              <Route
+                path="/cart"
+                element={<Cart />} />
+              <Route
+                path="/"
+                element={<Products />} />
+              <Route
+                path="/login"
+                element={<LoginForm
+                  get_token={(username, password) => this.get_token(username, password)}
+                />} />
+            </Routes>
+          </BrowserRouter>
+        </ApolloProvider>
       </div>
     );
   }
--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -1,6 +1,36 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import { useQuery, gql, } from '@apollo/client';
 
+
+const GET_PRODUCTS = gql`
+  query GetProducts {
+    allProducts {
+      id
+      title
+    }
+  }
+`;
+const ProductsItems = () => {
+    const { loading, error, data } = useQuery(GET_PRODUCTS);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error : {error.message}</p>;
+    console.log(data.allProducts)
+
+    return data.allProducts.map(({ id, title, image }) =>
+    (
+        <div className="col">
+            <div className="card">
+                <div className="card-body">
+                    <h3 className="card-title"><Link to={`/product/${id}`} className="card-link">{title}</Link></h3>
+                </div>
+                <img src="https://reber48.ru/wp-content/uploads/2021/09/285621.0x1000.gif" className="card-img-top" width='200px' height='200px' alt="img"></img>
+            </div>
+        </div>
+    )
+    )
+}
 class Products extends React.Component {
     constructor(props) {
         super(props)
@@ -8,16 +38,13 @@ class Products extends React.Component {
     }
 
     render() {
-        return (
-            <div className="container">
-                <div className="card-group">
-                    <div className="card">
-                        <div className="card-body">
-                            <h3 className="card-title"><Link to='/product/1' className="card-link">Товар 1</Link></h3>
-                        </div>
-                        <img src="https://reber48.ru/wp-content/uploads/2021/09/285621.0x1000.gif" className="card-img-top" width='200px' height='200px' alt="img"></img>
 
-                    </div>
+        return (
+
+            <div className="container">
+
+                <div className="row row-cols-1 row-cols-sm-4 g-3">
+                    <ProductsItems />
                 </div>
             </div>
         )
